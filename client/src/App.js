2cfcaa4ed9d938f5992f5599f656433e06646a43
dankarger <karger.dan@gmail.com>
2022-10-01 19:59:55 +0300

    adding HomePage.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React  from "react";
+import HomePage from "./HomePage/HomePage";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     APP
+    <HomePage />
     </div>
   );
 }
