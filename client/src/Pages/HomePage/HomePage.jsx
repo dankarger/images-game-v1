c8fdcaf1bd1bf7cfce5547c09f4697c87fda5918
dankarger <karger.dan@gmail.com>
2022-10-01 21:13:26 +0300

    adding  server api and strt function in client

--- client/src/Pages/HomePage/HomePage.jsx
+++ client/src/Pages/HomePage/HomePage.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import BasicButton from "../../components/Button/Button";
+import api from "../../Api/Api";
 
 const HomePage = () => {
-
-    const handleStartButtonClick=()=>{
+    const [image, setImage] = useState('')
+    const handleStartButtonClick=async ()=>{
         console.log('start')
+      const picture =   api.get('/picture?query=test')
+        console.log('pic',picture)
     }
 
 
--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const path = require("path");
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 const  app = express();
 const PORT = process.env.PORT || 8080 ;
-const pictureRoute = require('./route/picture.route')
+const pictureRoute = require('./routes/picture.route')
 
 let publicPath = path.join(__dirname, '../client/build')
 if(PORT===8080){
@@ -12,7 +12,7 @@ if(PORT===8080){
 }
 app.use(express.static(publicPath));
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 app.use('/api',pictureRoute)
 
 
