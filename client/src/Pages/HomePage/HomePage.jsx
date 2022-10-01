7cb89cbb5ed07bd71a9f4f2463f46faa41f15b42
dankarger <karger.dan@gmail.com>
2022-10-01 21:39:03 +0300

    adding pickRandomPicture function 2

--- client/src/Pages/HomePage/HomePage.jsx
+++ client/src/Pages/HomePage/HomePage.jsx
@@ -2,21 +2,31 @@ import React, {useState} from 'react'
 import BasicButton from "../../components/Button/Button";
 import api from "../../Api/Api";
 import {pickRandomPicture} from "../../utils/utils";
+import Picture from "../../components/Picture/Picture"
+import './HomePage.css'
 
 const HomePage = () => {
-    const [imageUrl, setImageUrl] = useState('')
+    const [imageUrl, setImageUrl] = useState('');
+    const [randomPicture, setRandomPicture] = useState({
+        src:{},
+
+    })
     const handleStartButtonClick=async ()=>{
         console.log('start')
       const picturesList =  await api.get('/picture?query=test')
-       const picture = pickRandomPicture(picturesList.data)
+       const picture = await pickRandomPicture(picturesList.data)
         console.log('pic',picture)
+        setImageUrl(picture.src.original)
+        setRandomPicture(picture)
     }
 
 
     return (
-        <div>
+        <div className='home-page'>
             HOMEPAGE
             <BasicButton label='start' theme={"outlined"} onclick={handleStartButtonClick} />
+            { imageUrl && <Picture url={randomPicture.src.original} title={randomPicture.alt}/> }
+            {/*<img src={imageUrl?.src.medium}/>*/}
         </div>
     )
 }
