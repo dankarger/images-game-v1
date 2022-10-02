b48f9145b12455481abdcee5c3facb321d4aa428
dankarger <karger.dan@gmail.com>
2022-10-02 12:44:59 +0300

    add title

--- client/src/Pages/HomePage/HomePage.jsx
+++ client/src/Pages/HomePage/HomePage.jsx
@@ -9,23 +9,31 @@ const HomePage = () => {
     const [imageUrl, setImageUrl] = useState('');
     const [randomPicture, setRandomPicture] = useState({
         src:{},
+        title:'raNDOM',
 
-    })
-    const handleStartButtonClick=async ()=>{
-        console.log('start')
-      const picturesList =  await api.get('/picture?query=test')
-       const picture = await pickRandomPicture(picturesList.data)
-        console.log('pic',picture)
+    });
+    // const [imgTitle, setImgTitle] = useState('test');
+
+
+    const handleStartButtonClick = async (query)=>{
+      console.log('start')
+      const picturesList =  await api.get(`/picture?query=${query}`)
+       const picture = await pickRandomPicture(picturesList.data);
+          // setImgTitle(prev=>query)
+        // console.log('pic',picture)
         setImageUrl(picture.src.original)
-        setRandomPicture(picture)
+        const pictureObj = {...picture,title2:query}
+        // setRandomPicture({...picture, title: query})
+        setRandomPicture(pictureObj)
+        console.log('title',pictureObj)
     }
 
 
     return (
         <div className='home-page'>
             HOMEPAGE
-            <BasicButton label='start' theme={"outlined"} onclick={handleStartButtonClick} />
-            { imageUrl && <Picture url={randomPicture.src.original} title={randomPicture.alt}/> }
+            <BasicButton label='start' theme={"outlined"} onclick={()=>handleStartButtonClick('test2')} />
+            { imageUrl.length >0 && <Picture url={randomPicture.src.original} title={randomPicture?.title2}/> }
             {/*<img src={imageUrl?.src.medium}/>*/}
         </div>
     )
