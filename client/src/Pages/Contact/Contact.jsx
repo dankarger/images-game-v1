7c278cd38cda4f63025dca71fb9a3384c511eacb
dankarger <karger.dan@gmail.com>
2022-10-06 23:55:14 +0300

    working on contact-page

--- client/src/Pages/Contact/Contact.jsx
+++ client/src/Pages/Contact/Contact.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import NavigationIcon from '@mui/icons-material/Navigation';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
 import './Contact.css'
 
-const Contact = () => {
+export default function Contact() {
     return (
         <div className='contact-page'>
-         Contact
+            <Box sx={{ '& > :not(style)': { m: 1 } }}>
+                <Fab color="primary" aria-label="add">
+                    <GitHubIcon />
+                </Fab>
+                <Fab color="secondary" aria-label="edit">
+                    <EmailIcon />
+                </Fab>
+                <Fab variant="extended">
+                    <NavigationIcon sx={{ mr: 1 }} />
+                    Navigate
+                </Fab>
+                <Fab disabled aria-label="like">
+                    <FavoriteIcon />
+                </Fab>
+            </Box>
         </div>
-    )
+    );
 }
-
-export default Contact
