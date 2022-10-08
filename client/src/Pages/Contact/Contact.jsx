4b4e27547a1288ead4ace2ea18c68baea66a5a69
dankarger <karger.dan@gmail.com>
2022-10-08 19:12:57 +0300

    deploy heroku

--- client/src/Pages/Contact/Contact.jsx
+++ client/src/Pages/Contact/Contact.jsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Fab from '@mui/material/Fab';
-import AddIcon from '@mui/icons-material/Add';
-import EditIcon from '@mui/icons-material/Edit';
+
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import NavigationIcon from '@mui/icons-material/Navigation';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import EmailIcon from '@mui/icons-material/Email';
 import './Contact.css'
@@ -12,19 +12,16 @@ import './Contact.css'
 export default function Contact() {
     return (
         <div className='contact-page'>
+            <h1>Contact Me</h1>
             <Box sx={{ '& > :not(style)': { m: 1 } }}>
                 <Fab color="primary" aria-label="add">
-                    <GitHubIcon />
-                </Fab>
-                <Fab color="secondary" aria-label="edit">
-                    <EmailIcon />
-                </Fab>
-                <Fab variant="extended">
-                    <NavigationIcon sx={{ mr: 1 }} />
-                    Navigate
+                    <a href="https://github.com/dankarger" target='_blank'> <GitHubIcon /> </a>
                 </Fab>
                 <Fab disabled aria-label="like">
-                    <FavoriteIcon />
+                    <a href="https://www.linkedin.com/in/dan-karger-ba36905a/" target={'_blank'}>    <LinkedInIcon /></a>
+                </Fab>
+                <Fab >
+                    <a href="mailto:dk.code.org@gmail.com"><EmailIcon /></a>
                 </Fab>
             </Box>
         </div>
