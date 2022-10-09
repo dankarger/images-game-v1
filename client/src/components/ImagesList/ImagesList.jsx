5db79014f0bfbae6428151ff9d18256d9b378c51
dankarger <karger.dan@gmail.com>
2022-10-09 15:15:30 +0300

    add style to BackDrop and ImageList

--- client/src/components/ImagesList/ImagesList.jsx
+++ client/src/components/ImagesList/ImagesList.jsx
@@ -8,75 +8,28 @@ export default function ImagesList({imagesList}) {
     const numberOfCol = imagesList.length === 4 ? 3 : 5;
     if (!imagesList) return null;
     return (
-        <ImageList sx={{width: 500, height: 450}} cols={numberOfCol} rowHeight={164}>
+        <ImageList sx={{width: 600, height: 450, overflowY:'unset'}} cols={numberOfCol} rowHeight={164}>
             {imagesList.map((item, index) => (
-                <ImageListItem key={item?.src?.small}>
+                <ImageListItem sx={{marginTop:10,width:100+'%'}} key={item?.src?.small}>
                     {index !== imagesList.length - 1 &&
+                    <a href={item.url} target='_blank'>
                     <img
                         src={`${item?.src?.small}?w=164&h=164&fit=crop&auto=format`}
                         srcSet={`${item?.src?.small}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
                         alt={item?.imgTitle}
                         loading="lazy"
                     />
+                        <p className={'image-title'}>{imagesList[index + 1]?.imgTitle}</p>
+
+                    </a>
+
                     }
-                    <p className={'image-title'}>{imagesList[index + 1]?.imgTitle}</p>
                     {imagesList[index + 1] &&
-                    <a className='image-credit' href={imagesList[index + 1]?.photographer_url}><p>Photo
-                        by: {imagesList[index + 1]?.photographer}</p></a>}
+                    <a className='image-credit' href={item?.photographer_url} target='_blank'><p>Photo
+                        by: {item?.photographer}</p></a>}
                 </ImageListItem>
 
             ))}
         </ImageList>
     );
 }
-
-const itemData = [
-    {
-        img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-        title: 'Breakfast',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-        title: 'Burger',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-        title: 'Camera',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-        title: 'Coffee',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-        title: 'Hats',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-        title: 'Honey',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-        title: 'Basketball',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-        title: 'Fern',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-        title: 'Mushrooms',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-        title: 'Tomato basil',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-        title: 'Sea star',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-        title: 'Bike',
-    },
-];
--- client/src/components/Instructions/Instructions.jsx
+++ client/src/components/Instructions/Instructions.jsx
@@ -4,15 +4,18 @@ import './Instructions.css'
 const Instructions=()=>{
     return(
         <div className='instructions'>
-            <h3 class={'heading'} data-text={'Instructions:'}>Instructions:</h3>
-            <h4> - The goal of the game is to say (or write) the first association when given a picture.</h4>
-            <h4> - The first picture given is random and the following is depending on your input.</h4>
-            <h4> - You can speak (after enabling the mic for this site ) or write (and press Enter) your answer. </h4>
-            <h4> - The faster you say or write your association the higher points you get. </h4>
-            <h4> - Try not to repeat your answers (it is harder than its sound).</h4>
-            <h4> - Choose How many images you want to play and press Start. </h4>
-            <h4>  -- The speech detection sometimes make mistake in capturing your speech,  but most of the time the result is  funny  and make part of the fun :) </h4>
-            <h4>---Have Fun---</h4>
+            <h3 className={'heading'} data-text={'Instructions:'}>Instructions:</h3>
+            <div className="text">
+                <h4> - The goal of the game is to say (or write) the first association when given a picture.</h4>
+                <h4> - The first picture given is random and the following is depending on your input.</h4>
+                <h4> - You can speak (after enabling the mic for this site ) or write (and press Enter) your answer. </h4>
+                <h4> - The faster you say or write your association the higher points you get. </h4>
+                <h4> - Try not to repeat your answers (it is harder than its sound).</h4>
+                <h4> - Choose How many images you want to play and press Start. </h4>
+                <h4>  -- The speech detection sometimes make mistake in capturing your speech,  but most of the time the result is  funny  and make part of the fun :) </h4>
+                <h4>  ---  Have Fun  ---  </h4>
+            </div>
+
         </div>
     )
 }
